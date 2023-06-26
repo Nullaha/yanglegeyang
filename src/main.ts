ca06d87041d466c1399faccd80c66e6808efb362
work <work@123.com>
2023-06-26 17:29:50 +0800

    init pinia 以及解决找不到xxx模块或其相应的类型声明

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,21 @@ import App from './App.vue'
 
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import { createPinia } from 'pinia'
+import piniaPersist from "pinia-plugin-persist";
+
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+
+const app = createApp(App)
+app
+  .use(router)
+  .use(ElementPlus)
+  
+const pinia = createPinia()
+pinia.use(piniaPersist)
+app.use(pinia)
+
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'  // 编辑器提示path模块找不到。下载 npm install @types/node --dev 就可
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({
+    include: [/\.vue$/],
+  })],
+  resolve:{
+    alias:{
+      '@':resolve(__dirname,'src') // 配置别名：将 @ 指向'src'目录
+    }
+  },
+  server:{
+    host: '0.0.0.0', // 解决vite use--host to expose
+    port: 5173, // 设置端口号
+    open: false // 服务启动后自动打开浏览器
+  },
 })
